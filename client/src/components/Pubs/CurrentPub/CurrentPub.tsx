94da38b9763983c54e3ab0bfce1402ffad68f279
Maikel van Veen <maikeloo10@hotmail.com>
2018-12-04 16:33:18 +0100

    feat(column): add more styling

--- client/src/components/Pubs/CurrentPub/CurrentPub.tsx
+++ client/src/components/Pubs/CurrentPub/CurrentPub.tsx
@@ -13,10 +13,13 @@ interface Props {
 export class CurrentPub extends React.Component<Props> {
     public render() {
         const properties = this.loopThroughPubProperties() || []
+        const pubNumber = 3
+        const randomBeerAmount = this.getArrayOfRandomNumbersWhichEqual(20)[pubNumber] || 1
+        console.log(this.getArrayOfRandomNumbersWhichEqual(20)[pubNumber])
 
         return (
             <div className={this.getClassName()}>
-                <BeerAmount amount={1}/>
+                <BeerAmount amount={randomBeerAmount}/>
                 <List>
                     {properties
                         .filter(([ key, value ]) => !!value)
@@ -34,6 +37,25 @@ export class CurrentPub extends React.Component<Props> {
                 </List>
             </div>
         )
+    }
+
+    // https://stackoverflow.com/questions/24020709/generate-an-array-that-adds-up-to-specific-value-using-only-2-numbers
+    private getArrayOfRandomNumbersWhichEqual = (maxTotal: number) => {
+        let sum = 0
+        const numbers = []
+
+        while (sum < maxTotal) {
+            const num = Math.floor((Math.random() * 2) + 4)
+            if (sum + num > maxTotal) {
+                numbers.push(1)
+                sum += 1
+            } else {
+                numbers.push(num)
+                sum += num
+            }
+        }
+
+        return numbers
     }
 
     private loopThroughPubProperties = () => {
