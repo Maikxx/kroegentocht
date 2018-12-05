1e588f4d3d73f97eb59b59563cbb3c1b2963d43e
Maikel van Veen <maikeloo10@hotmail.com>
2018-12-05 09:52:28 +0100

    feat(Pubs): add wip for changing the current pub

--- client/src/views/RootView.tsx
+++ client/src/views/RootView.tsx
@@ -8,34 +8,45 @@ import { BeerProgress } from '../components/Pubs/BeerProgress/BeerProgress'
 import { NextPubsList } from '../components/Pubs/NextPubsList/NextPubsList'
 import { PubsMap } from '../components/Pubs/PubsMap/PubsMap'
 import pubs from '../../assets/data/barsAndPubs.json'
+import { Pub } from '../types/Pub'
 
 interface Props {}
 
 interface State {
     beerProgress: number
+    selectedPub?: Pub
 }
 
 export class RootView extends React.Component<Props, State> {
     public state: State = {
         beerProgress: 0,
+        selectedPub: undefined,
     }
 
+    private filteredPubs: Pub[]
+
     public componentDidMount() {
+        const selectedPubIds = [ 'n2725878434', 'n1724352586', 'n1741897815', 'n1083668064', 'n1221258124' ]
+        this.filteredPubs = pubs.filter(pub => selectedPubIds.includes(pub.full_id))
+
+        this.setState({ selectedPub: this.filteredPubs[0] })
+
         window.addEventListener('click', () => {
-            this.setState({ beerProgress: this.state.beerProgress + 1 })
+            this.setState({ beerProgress: this.state.beerProgress + 1, selectedPub: this.filteredPubs[this.state.beerProgress + 1] })
         })
     }
 
     public render() {
-        const { beerProgress } = this.state
-        const pub = pubs[5]
+        const { beerProgress, selectedPub } = this.state
 
         return (
             <View>
                 <PageTitle />
                 <Section>
                     <Column title={`Huidige kroeg`}>
-                        <CurrentPub pub={pub} />
+                        {selectedPub && (
+                            <CurrentPub pub={selectedPub} />
+                        )}
                     </Column>
                     <Column>
                         <Column title={`Voortgang`}>
