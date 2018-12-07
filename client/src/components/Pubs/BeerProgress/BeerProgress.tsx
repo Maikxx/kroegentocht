52c1487d3dc9ea861beb9336c28bbadeef184e5f
Maikel van Veen <maikeloo10@hotmail.com>
2018-12-07 13:06:14 +0100

    feat(beerprogress): add input

--- client/src/components/Pubs/BeerProgress/BeerProgress.tsx
+++ client/src/components/Pubs/BeerProgress/BeerProgress.tsx
@@ -9,18 +9,46 @@ interface Props {
     progress: number
 }
 
-export class BeerProgress extends React.Component<Props> {
+interface State {
+    amountSet: boolean
+}
+
+export class BeerProgress extends React.Component<Props, State> {
+    public state: State = {
+        amountSet: false,
+    }
+
     public render() {
         const { progress } = this.props
+        const { amountSet } = this.state
 
         return (
             <div className={this.getClassName()}>
-                {times(progress).map((n: number) => (
-                    <BeerProgressIcon filled={true} key={n}/>
-                ))}
-                {times(20 - progress).map((n: number) => (
-                    <BeerProgressIcon filled={false} key={n}/>
-                ))}
+                {!amountSet && (
+                    <React.Fragment>
+                        <input
+                            type="number"
+                            className={`krt-BeerProgress__amount`}
+                            value={20}
+                        />
+                        <button
+                            onClick={() => this.setState({ amountSet: true })}
+                            className={`krt-BeerProgress__set-button`}
+                        >
+                            Set amount of beers
+                        </button>
+                    </React.Fragment>
+                )}
+                {amountSet && (
+                    <React.Fragment>
+                        {times(progress).map((n: number) => (
+                            <BeerProgressIcon filled={true} key={n}/>
+                        ))}
+                        {times(20 - progress).map((n: number) => (
+                            <BeerProgressIcon filled={false} key={n}/>
+                        ))}
+                    </React.Fragment>
+                )}
             </div>
         )
     }
